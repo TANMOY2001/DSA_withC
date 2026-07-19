ab5e40f8dbc9354401bb8d913017f904e6e28247
Tanmoy2001 <tanmoyghosh1018@gmail.com>
2026-07-20 02:11:23 +0530

    some exeriment

--- remDuplicate.c
+++ remDuplicate.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 //#include<conio.h>
 int main(){
-int arr[]={11,12,13,14,14,14,14,15,15,16,16,16,17,20,20,20,20,20,20,20};    //1,1,2,2,4,5,6,6,7,7,7,7};
+int arr[]={11,11,11,12,12,13,14,14,14,14,15,15,16,16,16,17,20,20,20,20,20,20,20};    //1,1,2,2,4,5,6,6,7,7,7,7};
 int i=0,j,k;
 int n = sizeof(arr)/sizeof(arr[0]);
-printf("single element size: %d\n",sizeof(arr)/sizeof(arr[0]));   //start arr size : %d",sizeof(arr)/sizeof(arr[0]));
-printf("sizeof Array- %d",sizeof(arr));
+//printf("single element size: %d\n",sizeof(arr)/sizeof(arr[0]));   //start arr size : %d",sizeof(arr)/sizeof(arr[0]));
+//printf("sizeof Array- %d",sizeof(arr));
 
 for (j=1;j<n;j++){
     //printf("%d",i);
@@ -17,7 +17,8 @@ for (j=1;j<n;j++){
         arr[i]=arr[j];
     }  
 }
-printf("%d\n",i);
+//arr=[i]; 
+printf("size of int %d\n",sizeof(int));
 for(k=0;k<=i;k++){
     printf("arr[%d] = %d\n",k,arr[k]);
 }
