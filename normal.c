0ff0442e58a7cac9f8edc2ef563434e207ddc922
Tanmoy2001 <tanmoyghosh1018@gmail.com>
2026-07-25 03:07:14 +0530

    updates

---
+++ normal.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    //int a=1;
+    //int *b=&a;
+    //*b=200;
+    //printf( " %p \n " ,&a);
+    //printf( " %p \n " ,b);
+    //printf( " %d \n " ,a);
+
+    int a=1;
+    int b=2,c=1;
+    printf( " %d \n " ,a+b+c);
+}
---
+++ sec_largest.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int i=0,j,k=0;
+    int arr[]={4,3,1,2};
+    int n=sizeof(arr)/sizeof(arr[0]);
+
+    for(j=1;j<n;j++){
+        if(arr[i]<arr[j]){
+            k=arr[i];
+            i=j;
+        
+        }
+        else if(arr[i]>arr[j]){
+            if(k<arr[j]){
+            k=arr[j];
+            }
+        }
+    
+    }
+    printf("out for The second largest element is %d",k);
+}
