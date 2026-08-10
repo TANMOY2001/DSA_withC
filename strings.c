228645ece20ae6e122d2936cf5e8c805e66eec40
Tanmoy2001 <tanmoyghosh1018@gmail.com>
2026-08-11 00:18:50 +0530

    palindrome

--- strings.c
+++ strings.c
@@ -6,32 +6,47 @@ int main(){
     
     // char arr[6]={'g','o','p','a','l','\0'};
     // printf("The array is:  %c\n",arr[1]);
-    
-    // return 0;
-
    //string input and length calculation
     // char name[10];
     // int i,count=0;
     // printf("Enter your name: ");
     // gets(name);
     // printf("The name is: %s\n",name);
-    ////using predefinned function strlen() to calculate length of string
+    // //using predefinned function strlen() to calculate length of string
     // int length = strlen(name);
-    // printf("length of name is :%d",length);
+    // // printf("length of name is :%d",length);
     // for(int i=0; name[i]!=0;i++){
     //     count++;
     // }
-    // printf("length of name is :%d",count);
+    // printf("(userdefined)length of name is :%d",count);
 
-    char s1[10];
-    printf("Enter your name: ");
-    gets(s1);
-    int i=0;
-    while(s1[i]!='\0'){
-        printf("name is :[%d] %c\n",i,s1[i]);
-        i++;
+    // char s1[10];
+    // printf("Enter your name: ");
+    // gets(s1);
+    // int i=0;
+    // while(s1[i]!='\0'){
+    //     printf("name is :[%d] %c\n",i,s1[i]);
+    //     i++;
 
 
+    // }
+//     return 0;
+// palindrome check
+char a[30];
+printf("Enter Text To Check Palindrome or Not: ");
+scanf("%s",a);
+int i=0;
+int j=strlen(a)-1;
+while (i<j)
+{
+    if (a[i] != a[j])
+    {
+        printf("!!! %c is Not Palindrome...\n",a);
+        return 0;
     }
-    return 0;
+    i++,j--; 
+}
+printf("%s is Palindrome...\n",a);
+
+
 }
